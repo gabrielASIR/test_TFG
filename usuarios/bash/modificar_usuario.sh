b9d664aea9447651173b72b6fee20fc502f42b8b
gabrielASIR <127197445+gabrielASIR@users.noreply.github.com>
2024-05-23 15:52:16 +0200

    Update modificar_usuario.sh

--- usuarios/bash/modificar_usuario.sh
+++ usuarios/bash/modificar_usuario.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Script para la modificación de usuarios
+
 # Recoger parámetros del formulario
 nombre_usuario="{nombre_usuario}"
 contrasena="{contrasena}"
 grupo_principal="{grupo_principal}"
 otros_grupos="{otros_grupos}"
 nombre_completo="{nombre_completo}"
-directorios="{directorios}"
 skel="{skel}"
 directorio_home="{directorio_home}"
 shell="{shell}"
@@ -17,13 +17,15 @@ inactive="{inactive}"
 crear_home="{crear_home}"
 copiar_skel="{copiar_skel}"
 
-# Mostrar los datos si existen
+# Generamos la contraseña formateada
+contrasena_formateada=$(mkpasswd -m sha-512 "$contrasena")
+
+# Mostramos los datos comprobando si existen
 echo "Nombre de usuario: $nombre_usuario"
-[ -n "$contrasena" ] && echo "Contraseña: $contrasena"
+[ -n "$contrasena" ] && echo "Contraseña: $contrasena_formateada"
 echo "Grupo principal: $grupo_principal"
 [ -n "$otros_grupos" ] && echo "Otros grupos: $otros_grupos"
 echo "Nombre completo: $nombre_completo"
-[ -n "$directorios" ] && echo "Directorios a crear: $directorios"
 [ -n "$skel" ] && echo "Directorio skel: $skel"
 [ -n "$directorio_home" ] && echo "Directorio home: $directorio_home"
 echo "Shell: $shell"
@@ -34,19 +36,20 @@ echo "Shell: $shell"
 [ -n "$crear_home" ] && echo "Crear directorio home: $crear_home"
 [ -n "$copiar_skel" ] && echo "Copiar archivos de skel: $copiar_skel"
 
-# Preguntar al usuario si desea confirmar la operación
+# Preguntamos al usuario final si desea confirmar la operación
 read -p "¿Desea modificar el usuario? (S/N): " confirmacion
 
-# Si la confirmación es "S", montar el comando con los datos introducidos
-if [ "$confirmacion" = "S" ]; then
+# Si la confirmación es "S", montamos el comando con los datos introducidos
+if [ "$confirmacion" = "S" ]
+then
     # Montaje de comando
     comando_usermod="usermod"
-    [ -n "$contrasena" ] && comando_usermod+=" -p $contrasena"
+    [ -n "$contrasena" ] && comando_usermod+=" -p $contrasena_formateada"
     [ -n "$grupo_principal" ] && comando_usermod+=" -g $grupo_principal"
     [ -n "$otros_grupos" ] && comando_usermod+=" -G $otros_grupos"
     [ -n "$nombre_completo" ] && comando_usermod+=" -c '$nombre_completo'"
-    [ -n "$directorios" ] && comando_usermod+=" -d $directorios"
     [ -n "$skel" ] && comando_usermod+=" -k $skel"
+    [ -n "$directorio_home" ] && comando_usermod+=" -d $directorio_home"
     [ -n "$shell" ] && comando_usermod+=" -s $shell"
     [ -n "$expire" ] && comando_usermod+=" -e $expire"
     [ -n "$uid" ] && comando_usermod+=" -u $uid"
