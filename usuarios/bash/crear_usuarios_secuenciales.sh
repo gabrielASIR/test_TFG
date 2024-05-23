adde31abaea9d448e1cb51bf80d0bdfc0b882341
gabrielASIR <127197445+gabrielASIR@users.noreply.github.com>
2024-05-23 13:55:05 +0200

    Update crear_usuarios_secuenciales.sh

--- usuarios/bash/crear_usuarios_secuenciales.sh
+++ usuarios/bash/crear_usuarios_secuenciales.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# Script para la creación de usuarios secuenciales
 # Recoger parámetros del formulario
 prefijo="{prefijo}"
-inicio="{inicio}"
-fin="{fin}"
+inicio={inicio}
+fin={fin}
 
-# Crear usuarios secuenciales
-for ((i=$inicio; i<=$fin; i++)); do
-    nombre_usuario="$prefijo$i"
-    useradd "$nombre_usuario"
-    echo "Usuario $nombre_usuario creado"
-done
+echo "Los usuarios empezarán por $prefijo$inicio y acabarán en $prefijo$fin"
 
+read -p "Esta seguro de crear todos estos usuarios: (s/n)" confirmacion
+if [ "confirmacion" != "s" ]
+then
+    echo "Operacion cancelada"
+else
+    # Crear usuarios secuenciales
+    for ((i=$inicio; i<=$fin; i++)); do
+        nombre_usuario="$prefijo$i"
+        useradd "$nombre_usuario"
+        echo "Usuario $nombre_usuario creado"
+    done
+fi
 echo "Usuarios creados con exito"
