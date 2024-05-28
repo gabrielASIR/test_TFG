2953259b7c186292f409ca60a55bb47df4988692
gabrielASIR <127197445+gabrielASIR@users.noreply.github.com>
2024-05-28 21:12:27 +0200

    Update copia_programada.sh

--- copias/bash/copia_programada.sh
+++ copias/bash/copia_programada.sh
@@ -2,14 +2,15 @@
 # Recogemos los argumentos pasados desde Django
 nombre_copia="{nombre_copia}"
 dias_semana="{dias_semana}"
-hora_ejecucion="{hora_ejecucion}"
+hora="{hora}"
+minutos="{minutos}"
 frecuencia="{frecuencia}"
 numero_ejecuciones="{numero_ejecuciones}"
 
 # Mostramos los datos por pantalla
 echo "Ruta de la copia: $nombre_copia"
 echo "Días de la semana: $dias_semana"
-echo "Hora de ejecución: $hora_ejecucion"
+echo "Hora de ejecución: $hora:$minutos"
 echo "Frecuencia: $frecuencia"
 echo "Número de ejecuciones: $numero_ejecuciones"
 
@@ -21,6 +22,6 @@ then
 fi
 
 # Añadimos la programación de la copia al cron
-echo "$hora_ejecucion * * $dias_semana $nombre_copia $frecuencia $numero_ejecuciones" >> /etc/crontab
+echo "$minutos $hora * * $dias_semana root $nombre_copia $frecuencia $numero_ejecuciones" >> /etc/crontab
 
 echo "Programación de copia \"$nombre_copia\" realizada correctamente."
